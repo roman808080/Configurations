c3e4017517b20bf5216a736cf61a64462b3ddb78
Roman Herasymenko <roman.herasymenko@gmail.com>
2022-02-19 17:54:20 +0100

    Adding additional examples for wsl.

--- attach-main-tmux-session.sh
+++ attach-main-tmux-session.sh
@@ -1,6 +1,8 @@
 #!/bin/env bash
 # The script is intended to use with Windows Terminal
 # E.g. ssh dev -t ./Configurations/attach-main-tmux-session.sh
+# E.g. wsl.exe -d Ubuntu -e bash -c '~/Configurations/attach-main-tmux-session.sh'
+# E.g. powershell wsl.exe -d Ubuntu -e bash -c '~/Configurations/attach-main-tmux-session.sh'
 
 # Going to the home directory to start the session in the home directory.
 cd ~
